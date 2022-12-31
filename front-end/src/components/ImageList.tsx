1b0bd9e3051d0a45dbe484a908cf7c888adf043a
kurakurakuda <kris.3618pcp@gmail.com>
2022-12-31 17:07:54 +0900

    Add image list to Image List Page with dummy data

--- front-end/src/components/ImageList.tsx
+++ front-end/src/components/ImageList.tsx
@@ -1,15 +1,42 @@
-import LumosProps from 'dto/LumosProps';
-import React, { useState } from 'react';
-import '../css/home.css';
+import React from 'react';
+import '../css/imageList.css';
 
 const ImageList = () => {
-  const [imageProps, setImage] = useState<LumosProps | undefined>(undefined);
+  const images = [
+    {
+      id: '0',
+      createdDateTs: '2022-12-31T07:33:09.842Z'
+    },
+    {
+      id: '1',
+      createdDateTs: '2022-12-31T07:33:09.842Z'
+    },
+    {
+      id: '2',
+      createdDateTs: '2022-12-31T07:33:09.842Z'
+    },
+    {
+      id: '3',
+      createdDateTs: '2022-12-31T07:33:09.842Z'
+    },
+    {
+      id: '4',
+      createdDateTs: '2022-12-31T07:33:09.842Z'
+    }
+  ];
 
-  return (
-    <div className="home">
-      ImageList <br />
+  const imageListTile = (id: string, date: string) => (
+    <div key="$id" className="list-tile">
+      {date}
     </div>
   );
+  const imageListTiles: JSX.Element[] = [];
+
+  images.forEach(e => {
+    imageListTiles.push(imageListTile(e.id, e.createdDateTs));
+  });
+
+  return <div className="image-list">{imageListTiles}</div>;
 };
 
 export default ImageList;
